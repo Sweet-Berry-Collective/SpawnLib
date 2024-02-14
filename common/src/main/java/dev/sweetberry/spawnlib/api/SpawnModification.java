66828d032aee28a7794d2dc820b867559fb5f9e5
Oliver-makes-code <oliver@olivermakesco.de>
2024-02-14 17:47:33 -0600

    Allow SpawnModification to read/write data

--- common/src/main/java/dev/sweetberry/spawnlib/api/SpawnModification.java
+++ common/src/main/java/dev/sweetberry/spawnlib/api/SpawnModification.java
@@ -1,11 +1,15 @@
 package dev.sweetberry.spawnlib.api;
 
+import net.minecraft.nbt.Tag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 
-@FunctionalInterface
 public interface SpawnModification {
     boolean modify(SpawnContext context);
+
+    void toTag(Tag nbt);
+
+    void fromTag(Tag nbt);
 
     default boolean isValidForSpawning(SpawnContext context, Level level, Vec3 pos) {
         var box = context.getPlayerBoundingBox(pos);
